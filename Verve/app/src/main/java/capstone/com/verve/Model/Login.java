d325377831eab4fae236fac68e7f7620159c37f8
tubleranel <jrrtuble@gmail.com>
2018-11-24 03:31:23 +0800

    Working Login

--- Verve/app/src/main/java/capstone/com/verve/Model/Login.java
+++ Verve/app/src/main/java/capstone/com/verve/Model/Login.java
@@ -1,6 +1,70 @@
 package capstone.com.verve.Model;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.*;
+
 public class Login {
+    Users users = new Users();
+    String patient = "Patient";
+
+    public void allowUserToLogin(EditText email, EditText password, Context context, FirebaseAuth auth) {
+        String emailAdd = email.getText().toString().trim();
+        String userPassword = password.getText().toString().trim();
+
+        if (emailAdd.isEmpty()) {
+            Toast.makeText(context, "Please Enter your email", Toast.LENGTH_LONG).show();
+        }
+        if (userPassword.isEmpty()) {
+            Toast.makeText(context, "Please Enter you password", Toast.LENGTH_LONG).show();
+        }
+
+        userLogin(emailAdd, userPassword, context, auth);
+
+    }
 
 
+    private void userLogin(String email, String password, final Context context, FirebaseAuth auth) {
+
+
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            DatabaseReference mRef = FirebaseDatabase.getInstance().getReference();
+
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                    mRef.child("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.exists()) {
+                                String role = dataSnapshot.child("role").getValue(String.class);
+                                if (role.equals(patient)) {
+                                    Toast.makeText(context, "patient", Toast.LENGTH_LONG).show();
+                                }
+                            }
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+
+
+                }
+            }
+        });
+    }
 }
+
+
--- Verve/app/src/main/java/capstone/com/verve/View/LoginActivity.java
+++ Verve/app/src/main/java/capstone/com/verve/View/LoginActivity.java
@@ -5,15 +5,24 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import capstone.com.verve.Model.Login;
 import capstone.com.verve.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Login login = new Login();
+    EditText etUsername, etPassword;
+    FirebaseAuth auth = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etxt_username);
+        etPassword = findViewById(R.id.etxt_password);
+
+        auth = FirebaseAuth.getInstance();
 
         setHintTextColor();
     }
@@ -30,11 +39,15 @@ public class LoginActivity extends AppCompatActivity {
             i = new Intent(this, ForgotPasswordActivity.class);
             startActivity(i);
         }
+
+        if (v.getId() == R.id.btn_login){
+            login.allowUserToLogin(etUsername, etPassword, LoginActivity.this, auth);
+        }
     }
 
     private void setHintTextColor(){
-        EditText etUsername = findViewById(R.id.et_username);
-        EditText etPassword = findViewById(R.id.et_password);
+        EditText etUsername = findViewById(R.id.etxt_username);
+        EditText etPassword = findViewById(R.id.etxt_password);
 
         etUsername.setHintTextColor(getResources().getColor(R.color.Pale));
         etPassword.setHintTextColor(getResources().getColor(R.color.Pale));
