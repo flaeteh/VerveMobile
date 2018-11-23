fa7c21137e88ba36856cbf87cb7a4d9ab48f5cbd
tubleranel <jrrtuble@gmail.com>
2018-11-24 06:02:27 +0800

    Updated

--- Verve/app/src/main/java/capstone/com/verve/View/ForumFeedActivity.kt
+++ Verve/app/src/main/java/capstone/com/verve/View/ForumFeedActivity.kt
@@ -46,10 +46,10 @@ class ForumFeedActivity : AppCompatActivity() {
         container.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabs))
         tabs.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(container))
 
-        fab.setOnClickListener { view ->
+        /*fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
-        }
+        }*/
 
     }
 
